d5918ed7184001837435727b53a9da9030103c40
pratyush <guptapratyush98@gmail.com>
2022-12-24 18:59:47 +0530

    - application added to serve REST API

--- api_shoppersite/views.py
+++ api_shoppersite/views.py
@@ -1,7 +1,7 @@
+from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decrotators import api_view
+from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getData(request):
-    return Response({'name': 'Dennis', 'age': 28})
-    
+    return Response({'name': 'Vishal', 'age': 28})
