69315a484975fe83393bd0bcdf001145b9e71351
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-26 17:46:09 +0530

    - refactor views to define new endpoints

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.apiOverview),
     path("customer_list/", views.customerList),
     path("customer_detail/<str:pk>/", views.customerDetail),
-    path("create_customer/", views.createCustomer),
-    path("update_customer/<str:pk>/", views.updateCustomer),
-    path("delete_customer/<str:pk>/", views.deleteCustomer),
+    path("create_customer/", views.CreateCustomer.as_view()),
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,4 +1,6 @@
+from rest_framework import permissions, status
 from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
 from .models import Customer
@@ -11,7 +13,6 @@ def apiOverview(request):
         "Customer List": "/customer_list/",
         "Detail Customer View": "/customer_detail/<str:pk>/",
         "Create Customer": "/create_customer/",
-        "Update Customer": "/update_customer/<str:pk>/",
     }
     return Response(api_overview)
 
@@ -30,24 +31,17 @@ def customerDetail(request, pk):
     return Response(serializer.data)
 
 
-@api_view(["POST"])
-def createCustomer(request):
-    serializer = CustomerSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+class CreateCustomer(CreateAPIView):
+    queryset = Customer.objects.all()
+    serializer = CustomerSerializer
+    permission_classes = [permissions.AllowAny]
 
+    def post(self, request, format=None):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET", "POST"])
-def updateCustomer(request, pk):
-    customer = Customer.objects.get(id=pk)
-    serializer = CustomerSerializer(data=request.data, instance=customer)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-@api_view(["DELETE"])
-def deleteCustomer(request, pk):
-    Customer.objects.get(id=pk).delete()
-    return Response()
+    def get_serializer_class(self):
+        return CustomerSerializer
